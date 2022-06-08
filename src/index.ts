79bbf3ba566d60eb808b2179309298ac3cea6c31
dskim <deasung.kim2622@gmail.com>
2022-06-08 20:49:05 +0900

    로직 수정 및 사전인터뷰 내용 추가

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,40 @@
 import {Island} from "./island";
 
 /**
- * 단일코드가 아닌 OOP구조로 요청. (예] 생성기, 쿼리처리자, 복사처리자, 파일관리자, 시리얼라이저 등)
- 섬 관리 시스템 개발
-
- a. m x n 의 격자를 생성 (m, n 모두 2 ~ 10000 사이의 정수 )
- b. generateIsland(n) n개의 점을 격자의 좌표에 삽입. (중복되지 않게 격자 점을 초과 하지 않게, 즉 중첩 불가능)
- generateIsland 를 여러번 호출 할수 있음, 다만 중복 되지 않음 으로 실제 할당된 갯수를 리턴
- c. removeIsland(n) n개 만큼의 좌표에서 섬을 제거, 남은 섬 만큼만 제거 가능, 위와 같이 실제 삭제된 점 갯수 리턴
- d. 위의 격자와 섬좌표를 파일에 read/write 처리
+ * 문제
+ * 요구사항
+   - 단일코드가 아닌 OOP구조로 요청. (예] 생성기, 쿼리처리자, 복사처리자, 파일관리자, 시리얼라이저 등) 섬 관리 시스템 개발
+   - a. m x n 의 격자를 생성 (m, n 모두 2 ~ 10000 사이의 정수 )
+   - b. generateIsland(n) n개의 점을 격자의 좌표에 삽입. (중복되지 않게 격자 점을 초과 하지 않게, 즉 중첩 불가능)
+        generateIsland 를 여러번 호출 할수 있음, 다만 중복 되지 않음 으로 실제 할당된 갯수를 리턴
+   - c. removeIsland(n) n개 만큼의 좌표에서 섬을 제거, 남은 섬 만큼만 제거 가능, 위와 같이 실제 삭제된 점 갯수 리턴
+   - d. 위의 격자와 섬좌표를 파일에 read/write 처리
  */
 
-let row = 2;
+let row = 100;
 let column = 3;
 
+const filePath = './test.txt';
 
 const island = new Island(row, column)
 
-island.generateIsland(3)
+let startNum:number = 0
 
 //여러 번 호출을 함수 자체는 여러번 콜해도 중복으로 안찍게 하였습니다만 이게 맞는지 모르겠습니다.
-// island.generateIsland(3)
-// island.generateIsland(3)
+while (startNum < row * column) {
+
+  island.generateIsland(startNum)
+  startNum++
+
+}
+
 
 // island.removeIsland(4)
+// console.log(island.getGrid())
 
-console.log(island.getGrid())
+island.writeFile(filePath)
 
-
-
-
-
+island.asyncReadFile(filePath).then(r => console.log(r))
 
 
 
--- src/island.ts
+++ src/island.ts
@@ -1,30 +1,33 @@
+import {writeFileSync, promises as fsPromises} from 'fs';
+import {join} from 'path';
+
+const min:number = 2;
+const max:number = 10000;
+
 export class Island {
 
+  //격자 입력받은 m,n으로 2차원배열 생성
   private readonly grid: string[][]
 
   //a. m x n 의 격자를 생성 (m, n 모두 2 ~ 10000 사이의 정수 )
-  constructor(n:number,m:number) {
+  constructor(m: number, n: number) {
 
-    if (!(n >= 2 && n <= 10000) || !(m >= 2 && m <= 10000)) {
+    if (!(m >= min && m <= max) || !(n >= min && n <= max)) {
       throw new Error('2~10000 사이의 정수를 입력하세요');
     }
 
-    this.grid = Array.from(Array(n), ()=> Array(m).fill(""));
-  }
-
-  getGrid = () => {
-    return this.grid
+    this.grid = Array.from(Array(m), () => Array(n).fill(""));
   }
 
   // b. generateIsland(n) n개의 점을 격자의 좌표에 삽입. (중복되지 않게 격자 점을 초과 하지 않게, 즉 중첩 불가능)
   // generateIsland 를 여러번 호출 할수 있음, 다만 중복 되지 않음 으로 실제 할당된 갯수를 리턴
-  generateIsland = (num:number) => {
+  generateIsland = (num: number) => {
     // console.log(`num:${num}`)
     // console.log(`grids:${this.grid}`)
 
-    let tempNum:number = num
+    let tempNum: number = num
 
-    let realCnt:number = 0
+    let realCnt: number = 0
 
     //점이 안찍혀 있는 인덱스 추출
     this.grid.forEach(item => {
@@ -33,7 +36,7 @@ export class Island {
 
       if (tempNum == 0) return
 
-      for(let i = 0; i < item.length; i++) {
+      for (let i = 0; i < item.length; i++) {
 
         if (item[i] == '') {
           item[i] = '·'
@@ -53,17 +56,17 @@ export class Island {
   }
 
   //c. removeIsland(n) n개 만큼의 좌표에서 섬을 제거, 남은 섬 만큼만 제거 가능, 위와 같이 실제 삭제된 점 갯수 리턴
-  removeIsland = (num:number) => {
+  removeIsland = (num: number) => {
 
-    let tempNum:number = num
+    let tempNum: number = num
 
-    let deleteCnt:number = 0
+    let deleteCnt: number = 0
 
     this.grid.forEach(item => {
 
       if (tempNum == 0) return
 
-      for(let i = 0; i < item.length; i++) {
+      for (let i = 0; i < item.length; i++) {
 
         if (item[i] == '·') {
           item[i] = ''
@@ -82,8 +85,30 @@ export class Island {
   }
 
 
-  //d. 위의 격자와 섬좌표를 파일에 read/write 처리
+  //d. 위의 격자와 섬좌표를 파일에 write 처리
+  writeFile = (filename: string) => {
 
+    writeFileSync(join(__dirname, filename), JSON.stringify(this.grid), {
+      flag: 'w',
+    });
 
+  }
+
+  //d. 위의 격자와 섬좌표를 파일에 read 처리
+  async asyncReadFile(filename: string) {
+    try {
+      const result = await fsPromises.readFile(
+          join(__dirname, filename),
+          'utf-8',
+      );
+
+      // console.log(result);
+
+      return result;
+    } catch (err) {
+      console.log(err);
+      return '파일을 읽는데에 실패하였습니다.'
+    }
+  }
 
 }
